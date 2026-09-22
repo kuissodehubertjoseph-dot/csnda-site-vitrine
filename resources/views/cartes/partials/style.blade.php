42fd02ae13fbe6c3c60e0ddd3ffc6a74f79a4d2d
kuissodehubertjoseph-dot <kuissodehubertjoseph@gmail.com>
2026-09-22 16:28:34 +0100

    Retire le fond blanc du logo sur la carte UCAO/EGEI

--- resources/views/cartes/partials/style.blade.php
+++ resources/views/cartes/partials/style.blade.php
@@ -617,8 +617,6 @@
         height: 9mm;
         border-radius: 50%;
         overflow: hidden;
-        background: var(--color-white);
-        border: 0.25mm solid var(--color-white);
         margin-top: 0.2mm;
     }
 
