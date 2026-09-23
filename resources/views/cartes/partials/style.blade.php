63e740f1c3b78fafb3903c0a506a4664de524ee4
kuissodehubertjoseph-dot <kuissodehubertjoseph@gmail.com>
2026-09-23 10:50:57 +0100

    Ameliore la visibilite du nom de l'ecole en bas: bleu plus clair + ombre portee

--- resources/views/cartes/partials/style.blade.php
+++ resources/views/cartes/partials/style.blade.php
@@ -807,14 +807,16 @@
         font-size: 1.9mm;
         font-weight: 800;
         letter-spacing: 0.2px;
-        color: #4fb3f0;
+        color: #8fe0ff;
+        text-shadow: 0 0.15mm 0.3mm rgba(0, 0, 0, 0.55);
     }
 
     .ucao-pied-ligne2 {
         font-size: 1.6mm;
         font-weight: 700;
         letter-spacing: 0.2px;
-        color: #4fb3f0;
+        color: #8fe0ff;
+        text-shadow: 0 0.15mm 0.3mm rgba(0, 0, 0, 0.55);
         margin-top: 0.2mm;
     }
 
